fd48e6b588ff801fa30f78b58823ed3356b3d5a1
rohits-web03 <rohit.dey302003@gmail.com>
2023-09-21 20:36:14 +0530

    Create Menuitemsjsx

---
+++ src/Components/Menuitems.jsx
@@ -0,0 +1,14 @@
+export const MenuItems =[
+    {
+        title:"About",
+        url:"/about",
+        cName:"nav-links",
+        icon:"fa-solid fa-circle-info"
+    },
+    {
+        title:"Contact",
+        url:"/contact",
+        cName:"nav-links",
+        icon:"fa-solid fa-address-book"
+    },
+]
